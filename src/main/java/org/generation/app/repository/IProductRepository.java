d14442ab7a7558e6f362e9ad8fe78b5fb3fc76a1
Julio Hernández <julioergocesar@outlook.com>
2022-08-15 15:41:26 -0500

    ProductRestController y properties

--- src/main/java/org/generation/app/repository/IProductRepository.java
+++ src/main/java/org/generation/app/repository/IProductRepository.java
@@ -1,8 +1,11 @@
 package org.generation.app.repository;
 
+import java.util.List;
+
+import org.generation.app.entity.Order;
 import org.generation.app.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IProductRepository extends CrudRepository<Product, Long> {
-
+	
 }
