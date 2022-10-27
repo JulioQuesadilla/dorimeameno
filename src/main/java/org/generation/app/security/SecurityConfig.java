d4fff03fb5f29470442b6e9dc6b7c9578b675ef0
Julio Hernández <julioergocesar@outlook.com>
2022-10-27 03:10:25 -0500

    para cambiar laptop

--- src/main/java/org/generation/app/security/SecurityConfig.java
+++ src/main/java/org/generation/app/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package org.generation.app.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -33,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	/*Método de Seguridad */
 	@Override
 	  protected void configure(HttpSecurity http) throws Exception {
+		/*
 	    http
 	      .httpBasic().and()
 	      .authorizeRequests()
@@ -40,9 +43,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	        .antMatchers(HttpMethod.PUT, "/api/**").hasRole("USER")
 	        .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
 	        .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN","SAYAJIN")
+	        
+	        .antMatchers("/**").permitAll() //Luego de las restricciones, se permite todo.
+	        .anyRequest().authenticated() //Cualquier otra petición, requere autentificación
 	        .and().formLogin().permitAll()
 	        .and()
 	      .csrf().disable();
+	      */
+		http
+		  .csrf().disable()
+	      .authorizeRequests() 
+	        //.antMatchers("/**").permitAll()
+	        .anyRequest().authenticated() //Cualquier otra petición, requiere autentificación
+	        .and()
+	        .formLogin().permitAll()
+	        .and()
+	        //Las peticiones para /login pasará por este filtro, para darnos el token
+	        .addFilterBefore(new LoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+	        
+	      //El siguiente filtro, valorará en las peticiones http, si el token es correcto
+	        .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
+
 	  }
 
 }
